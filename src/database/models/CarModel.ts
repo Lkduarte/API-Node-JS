83c0788e04c71e8965550c0bd146c5d2ae0919ff
Lucas Nunes <lucas.nascimento72@fatec.sp.gov.br>
2022-10-27 00:13:57 -0300

    Server, Routes, Controller and Model

---
+++ src/database/models/CarModel.ts
@@ -0,0 +1,20 @@
+import { Schema } from 'mongoose'
+import mongoose from "mongoose";
+
+const CarModel = new Schema({
+    
+  marca: String,
+  modelo: String,
+  versao: String,
+  ano: Number,
+  quilometragem: Number,
+  tipoCambio: String,
+  precoVenda: Number
+
+},
+{
+  timestamps: true
+
+})
+
+export default mongoose.model('Car', CarModel)
